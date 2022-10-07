c78b08399944ef54cd15145ac1bd4e03bbe0740e
yunliang-ding <804408086@qq.com>
2022-10-07 17:30:44 +0800

    init

--- src/file-search/types.ts
+++
@@ -1,7 +0,0 @@
-import { FileExplorerProps } from '@/file-explorer/types';
-
-export interface FileSearchProps extends FileExplorerProps {
-  onSearch?: (keyword) => any;
-  exclude?: string[];
-  include?: string[];
-}
